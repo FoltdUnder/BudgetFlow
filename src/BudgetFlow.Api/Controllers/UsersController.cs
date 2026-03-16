99b619cf9198d68bbbcd242cbf20927f55d64015
John Doe <twinker7131@gmail.com>
2026-03-16 23:19:34 +0400

    fix missing import

--- src/BudgetFlow.Api/Controllers/UsersController.cs
+++ src/BudgetFlow.Api/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 using BudgetFlow.Application.Services;
+using BudgetFlow.Application.Interfaces;
 using BudgetFlow.Domain.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
